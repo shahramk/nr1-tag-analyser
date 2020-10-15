e205239296e686c7e3a142e117b1360fdcebfb3a
Anna Strom-Olsen <aso1124@gmail.com>
2020-10-15 11:39:41 -0400

    feat: make tag listing color blind friendly
    Add symbols to indicate the status of a tag in the tag listing, so that color is not the only means used to define status

--- nerdlets/tag-analyser-nerdlet/components/EntityTable/TagListing.js
+++ nerdlets/tag-analyser-nerdlet/components/EntityTable/TagListing.js
@@ -1,28 +1,55 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 const TagListing = ({ type, tags }) => {
-  const getStatus = (values) => {
-    const hasValue = values && values.find((v) => v !== '<undefined>');
-    if (hasValue) return 'high__band';
-    if (type === 'mandatory' && !hasValue) return 'low__band';
-    if (type === 'optional' && !hasValue) return 'mid__band';
-  };
+  const passes = []
+  const fails = []
+
+  tags.forEach(tag => {
+    const hasValue = tag.tagValues && tag.tagValues.find((v) => v !== '<undefined>');
+    if (hasValue) {
+      tag.status = 'high__band';
+      passes.push(tag);
+    }
+    if (type === 'mandatory' && !hasValue) {
+      tag.status = 'low__band';
+      fails.push(tag);
+    }
+    if (type === 'optional' && !hasValue) {
+      tag.status = 'mid__band';
+      fails.push(tag);
+    }
+  });
+
+  const renderTags = (typedTags) => {
+    return (
+      <>
+        {typedTags.map((tag, idx) => {
+          return (
+            <React.Fragment key={idx}>
+              <div className={`tags__tag ${tag.status}`}>
+                <label className="tags__tag__status">{`${tag.status === 'high__band'
+                ? '✓'
+                : tag.status === 'mid__band'
+                  ? '⚠️'
+                  : 'X'
+              }`}</label>
+                <label>{`${tag.tagKey}: ${tag.tagValues.join(', ')}`}</label>
+              </div>
+            </React.Fragment>
+          );
+        })}
+      </>
+    )
+  }
 
   return (
     <div>
       <label className="tags__title">{type}</label>
       <div className="tags__list">
-        {tags.map((tag, idx) => {
-          return (
-            <label
-              key={idx}
-              className={`tags__tag ${getStatus(tag.tagValues)}`}
-            >
-              {`${tag.tagKey}: ${tag.tagValues.join(', ')}`}
-            </label>
-          );
-        })}
+        {renderTags(fails)}
+        {renderTags(passes)}
       </div>
     </div>
   );
