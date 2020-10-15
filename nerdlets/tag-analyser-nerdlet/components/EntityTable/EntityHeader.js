6da02afb0f38bcb27a9b926a277eb12a5c5c2786
Anna Strom-Olsen <aso1124@gmail.com>
2020-10-15 11:40:09 -0400

    feat: update pdf style

--- nerdlets/tag-analyser-nerdlet/components/EntityTable/EntityHeader.js
+++ nerdlets/tag-analyser-nerdlet/components/EntityTable/EntityHeader.js
@@ -4,51 +4,80 @@ import PropTypes from 'prop-types';
 import { Button } from 'nr1';
 import PdfGenerator from '../PdfGenerator';
 
-const EntityHeader = ({ filter, count, accounts, entityType, entities }) => {
-  return (
-    <div className="entity__table__header__panel">
-      <div className="entity__table__header__message">
-        {`Showing ${count}${
-          entityType !== 'None' ? ` ${entityType} ` : ''} 
+export default class EntityHeader extends React.Component {
+  state = {
+    generatingPdf: false,
+  }
+
+  onGeneratePdf = () => this.setState({ generatingPdf: true });
+  onPdfDownload = () => this.setState({ generatingPdf: false });
+
+  render() {
+    const { count, entityType, accounts, filter, entities } = this.props;
+    const { generatingPdf } = this.state;
+
+    return (
+      <div className="entity__table__header__panel">
+        <div className="entity__table__header__message">
+          {`Showing ${count}${entityType !== 'None' ? ` ${entityType} ` : ''} 
           ${count > 1 ? 'entities' : 'entity'} 
           ${!accounts ? '' : `for account(s) ${accounts}`} `}
-      </div>
-      <div className="button__row">
-        <Button
-          onClick={() => filter('FULL')}
-          type={Button.TYPE.NORMAL}
-          iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__SHOW}
-          sizeType={Button.SIZE_TYPE.SMALL}
-        >
-          All Entities
-        </Button>
+        </div>
+        <div className="button__row">
+          <Button
+            onClick={() => filter('FULL')}
+            // disabled={generatingPdf}
+            type={Button.TYPE.PLAIN_NEUTRAL}
+            iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__SHOW}
+            sizeType={Button.SIZE_TYPE.SMALL}
+          >
+            All
+          </Button>
 
-        <Button
-          onClick={() => filter('OUT_OF_COMPLIANCE')}
-          type={Button.TYPE.NORMAL}
-          iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__HIDE_OTHERS}
-          sizeType={Button.SIZE_TYPE.SMALL}
-        >
-          Out of Compliance
-        </Button>
+          <Button
+            onClick={() => filter('OUT_OF_COMPLIANCE')}
+            // disabled={generatingPdf}
+            type={Button.TYPE.PLAIN_NEUTRAL}
+            iconType={Button.ICON_TYPE.INTERFACE__SIGN__TIMES__V_ALTERNATE}
+            sizeType={Button.SIZE_TYPE.SMALL}
+          >
+            Out of Compliance
+          </Button>
 
-        <Button
-          onClick={() => filter('IN_COMPLIANCE')}
-          type={Button.TYPE.NORMAL}
-          iconType={Button.ICON_TYPE.HARDWARE_AND_SOFTWARE__SOFTWARE__LIVE_VIEW}
-          sizeType={Button.SIZE_TYPE.SMALL}
-        >
-          In Compliance
-        </Button>
-        <PdfGenerator
-          data={entities}
-          accounts={!accounts ? 'All Accounts' : accounts}
-          filters={entityType}
-        />
+          <Button
+            onClick={() => filter('IN_COMPLIANCE')}
+            // disabled={generatingPdf}
+            type={Button.TYPE.PLAIN_NEUTRAL}
+            iconType={
+              Button.ICON_TYPE.INTERFACE__SIGN__CHECKMARK__V_ALTERNATE
+            }
+            sizeType={Button.SIZE_TYPE.SMALL}
+          >
+            In Compliance
+          </Button>
+          {generatingPdf ? (
+            <PdfGenerator
+              data={entities}
+              accounts={!accounts ? 'All Accounts' : accounts}
+              filters={entityType}
+              pdfComplete={this.onPdfDownload}
+            />
+          ) : (
+            <Button
+              onClick={() => this.onGeneratePdf()}
+              // disabled={generatingPdf}
+              type={Button.TYPE.PLAIN_NEUTRAL}
+              iconType={Button.ICON_TYPE.DOCUMENTS__DOCUMENTS__NOTES}
+              sizeType={Button.SIZE_TYPE.SMALL}
+            >
+              Generate pdf
+            </Button>
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 EntityHeader.propTypes = {
   filter: PropTypes.func.isRequired,
@@ -58,4 +87,4 @@ EntityHeader.propTypes = {
   entities: PropTypes.array.isRequired,
 };
 
-export default EntityHeader;
+// export default EntityHeader;
